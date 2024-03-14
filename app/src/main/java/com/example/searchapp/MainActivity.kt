3e4f852f50f70d267ca14c77e7e7a9f3d666cab0
r-elopre <donvgdong052@gmail.com>
2024-03-14 18:20:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/searchapp/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.searchapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.searchapp.ui.theme.SearchAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SearchAppTheme {
+                val viewModel = viewModel<MainViewModel>()
+                val searchText by viewModel.searchText.collectAsState()
+                val persons by viewModel.persons.collectAsState()
+                val isSearching by viewModel.isSearching.collectAsState()
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ) {
+                    TextField(value = searchText, onValueChange = viewModel::onSearchTextChange,
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = { Text(text = "search") })
+                    Spacer(modifier = Modifier.height(16.dp))
+                    if (isSearching) {
+                        Box(modifier = Modifier.fillMaxSize()) {
+                            CircularProgressIndicator(
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+                    } else {
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f)
+                        ) {
+                            items(persons) { person ->
+                                Text(
+                                    text = "${person.firstName} ${person.lastName}",
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(vertical = 16.dp)
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/searchapp/MainViewModel.kt
@@ -0,0 +1,95 @@
+package com.example.searchapp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+
+class MainViewModel: ViewModel() {
+
+    private val _searchText = MutableStateFlow("")
+    val searchText = _searchText.asStateFlow()
+
+    private val _isSearching = MutableStateFlow(false)
+    val isSearching = _isSearching.asStateFlow()
+
+    private val _persons = MutableStateFlow(allPersons)
+    val persons = searchText
+        .debounce(1000L)
+        .onEach { _isSearching.update { true } }
+        .combine(_persons){ text, persons ->
+            if (text.isBlank()) {
+                persons
+            } else {
+                delay(2000L)
+                persons.filter {
+                    it.doesMatchSearchQuery(text)
+                }
+            }
+        }
+        .onEach { _isSearching.update { false } }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(2000),
+            _persons.value
+        )
+
+    fun onSearchTextChange(text: String){
+        _searchText.value = text
+    }
+}
+
+data class Person(
+    val firstName:String,
+    val lastName:String
+){
+    fun doesMatchSearchQuery(query: String): Boolean{
+        val matchingCombinations = listOf(
+            "$firstName$lastName",
+            "$firstName $lastName",
+            "${firstName.first()} ${lastName.first()}",
+        )
+        return matchingCombinations.any {
+            it.contains(query, ignoreCase = true)
+        }
+    }
+}
+
+private val allPersons = listOf(
+    Person(
+        firstName = "Carl",
+        lastName = "Grimes"
+    ),
+    Person(
+        firstName = "Rick",
+        lastName = "Grimes"
+    ),
+    Person(
+        firstName = "Rodolfu",
+        lastName = "Mustafa"
+    ),
+    Person(
+        firstName = "Cain",
+        lastName = "Shell"
+    ),
+    Person(
+        firstName = "Albert",
+        lastName = "Einstein"
+    ),
+    Person(
+        firstName = "Nikola",
+        lastName = "Tesla"
+    ),
+    Person(
+        firstName = "Shim",
+        lastName = "Jin-woo"
+    ),
+
+)
